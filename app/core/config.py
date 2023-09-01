f06e0500224481e50b34a5cd6714d7c0eeb92535
justintroy <xermasterz@gmail.com>
2023-09-01 16:49:31 +0000

    changed db driver msql->postgresql

--- app/core/config.py
+++ app/core/config.py
@@ -16,20 +16,21 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
 
-    MYSQL_USER: str
-    MYSQL_PASSWORD: str
-    MYSQL_HOST: str
-    MYSQL_PORT: str
-    MYSQL_DATABASE: str
-    DATABASE_URI: Optional[str] = None
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_DATABASE: str
+    DB_DATABASE_TESTING: str
+    DATABASE_URI: Optional[str]
 
     @validator("DATABASE_URI", pre=True)
     def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
         if isinstance(v, str):
             return v
-        return f"mysql://{values.get('MYSQL_USER')}:{values.get('MYSQL_PASSWORD')}@{values.get('MYSQL_HOST')}:" \
-               f"{values.get('MYSQL_PORT')}/{values.get('MYSQL_DATABASE')}"
-    
+        return f"postgresql://{values.get('DB_USER')}:{values.get('DB_PASSWORD')}@{values.get('DB_HOST')}:" \
+               f"{values.get('DB_PORT')}/{values.get('DB_DATABASE')}"
+
     AUTH_SECRET_KEY: str
     AUTH_ALGORITHM: str
     AUTH_ACCESS_TOKEN_EXPIRE_MINUTES: int
--- app/database.py
+++ app/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import (
-    as_declarative,
     declared_attr,
     declarative_base,
 )
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, as_declarative
 
 from app.core.config import settings
 
