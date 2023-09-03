7a69cf24ead681d179f8c8d47e07344369449baa
justintroy <xermasterz@gmail.com>
2023-09-03 03:42:58 +0000

    added prod deployment pipeline w/ acr using Docker

--- app/api/symptoms/routes.py
+++ app/api/symptoms/routes.py
@@ -1,16 +1,5 @@
-from datetime import datetime, timedelta
-from typing import List, Optional
-
-import app.api.user.controller as user_controller
-import app.api.symptoms.controller as symptoms_controller
 from app.api.symptoms.symptoms_service import SymptomsService
-from app.api.user.schemas import User, UserCreate, UserUpdate
-from app.core.config import settings
-from app.core.schemas import Token
-from app.database import get_db
-from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
+from fastapi import APIRouter
 
 router = APIRouter()
 
